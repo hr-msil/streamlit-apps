c305dfc8b99e0fa7a4fb67a65e6da50b67da709a
kennobith <97459261+kennobith@users.noreply.github.com>
2026-01-06 12:35:52 -0300

    bug fix, logical or

--- mensualizados/mensualizados_.py
+++ mensualizados/mensualizados_.py
@@ -99,7 +99,7 @@ else:
 
         df = pd.read_excel(archivo_1)
         # 1. Filtrar filas donde la columna no esté vacía
-        df = df[df["Fecha Egreso Cargo"].notna() & (df["Fecha Egreso Cargo"] != "")]
+        df = df[df["Fecha Egreso Cargo"].notna() | (df["Fecha Egreso Cargo"] != "")]
         
         # 2. Reemplazar en Categoría
         df["Categoría"] = df["Categoría"].replace("NO CATEGORIZADO", 999)
@@ -171,5 +171,6 @@ else:
                 file_name=nombre_archivo_i,
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
+
 
 
